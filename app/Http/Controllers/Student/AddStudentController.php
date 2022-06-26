1cb0bf45597fe9a53100f12276e82a969d2c8fba
AbdULrahem <abdulrahemalz1@gmail.com>
2022-06-26 21:19:57 +0300

    a

--- app/Http/Controllers/Student/AddStudentController.php
+++ app/Http/Controllers/Student/AddStudentController.php
@@ -8,7 +8,6 @@ use App\Models\Paarent;
 use App\Models\Student;
 use App\Traits\basicFunctionsTrait;
 use App\Traits\generalTrait;
-use App\Traits\basicFunctionsTrait;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\File;
 use Illuminate\Support\Facades\Storage;
